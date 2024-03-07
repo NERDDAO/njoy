bff45edbca0b531f5f53c6945378633a3d755b68
at0x123 <vp.ca94@gmail.com>
2024-03-07 18:25:25 -0500

    fix

--- packages/nextjs/.next/server/server-reference-manifest.js
+++
@@ -1,5 +0,0 @@
-<<<<<<< HEAD
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"mAqcfRHGde4P4/1PLMl8jAh2Vt8TBcXsMViiM+/U0Nk=\"\n}"
-=======
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"pJQHatJ9PnIr5Zjp6Sst0z97crvAGndLJp+tPqdthFw=\"\n}"
->>>>>>> d0700bc (Window component page)
