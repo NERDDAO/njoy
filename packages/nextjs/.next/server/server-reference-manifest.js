8a6ad406833a7282e7b1ca79529dd910bc83771e
0xsks <prehistorictomatoes@gmail.com>
2024-03-07 13:20:40 -0800

    html-loader

---
+++ packages/nextjs/.next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"zC4lKexyAmwSxV+7lszu1kwPxPIDAMsdEN9DmCTjaRA=\"\n}"
--- packages/nextjs/next.config.js
+++ packages/nextjs/next.config.js
@@ -9,9 +9,17 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: process.env.NEXT_PUBLIC_IGNORE_BUILD_ERROR === "true",
   },
-  webpack: config => {
+  webpack: (config) => {
+    // Existing webpack config modifications
     config.resolve.fallback = { fs: false, net: false, tls: false };
     config.externals.push("pino-pretty", "lokijs", "encoding");
+
+    // Add the rule for html-loader
+    config.module.rules.push({
+      test: /\.html$/i,
+      loader: "html-loader",
+    });
+
     return config;
   },
 };
