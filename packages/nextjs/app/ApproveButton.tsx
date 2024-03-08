ae16d51a8dceb5accb070552a6706d2d670e2eb2
raigal-r <raquel.raigal@outlook.es>
2024-03-08 13:15:59 -0300

    buttons center

--- packages/nextjs/app/ApproveButton.tsx
+++ packages/nextjs/app/ApproveButton.tsx
@@ -44,11 +44,11 @@ const ApproveButton: React.FC<ApproveButtonProps> = ({ contractName, spenderAddr
   };
 
   return (
-    <div className="relative flex flex-col snap-center  w-full">
+    <div className="relative flex flex-col snap-center  w-full self-center justify-self-center">
       <div className=" justify-items-center h-full w-full flex flex-col snap-center relative top-0">
         <div className="relative bg-[url(/enjoy.png)] bg-cover bg-no-repeat h-24 w-52 top-0 left-1/2 -ml-20" />
         <div className="relative bg-[url(/enjoy.png)] bg-cover bg-no-repeat h-64 w-52 top-0 left-1/2 -ml-20"></div>
-        <ul className="relative">
+        <ul className="relative self-center">
           <button
             className="bg-no-repeat h-24 w-52 bottom-0  border-2 bg-cover p-4 bg-[url(/vwave.jpg)] text-blue-500"
             onClick={handleApprove}
@@ -58,8 +58,8 @@ const ApproveButton: React.FC<ApproveButtonProps> = ({ contractName, spenderAddr
           </button>
         </ul>
 
-        <label className="w-80 text-rose-700 bg-sky-400 my-4 pb-5 rounded-lg border-4 ">
-          Amount<input className="text-sky-400 bg-rose-700 mx-4"></input>
+        <label className="w-80 text-rose-700 bg-no-repeat bg-cover bg-[url(/vwave.jpg)] my-4 pb-5 rounded-lg border-4 self-center ">
+          Amount<input className="text-sky-400 bg-no-repeat mx-4"></input>
         </label>
       </div>
     </div>
