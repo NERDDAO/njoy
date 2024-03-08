81e91f01a87e4c10993cbfec1fbbd23673cc99d9
at0x123 <vp.ca94@gmail.com>
2024-03-08 09:37:56 -0500

    howl

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -8,9 +8,16 @@ import Dropup from "~~/components/DropUp";
 import MintWindow from "~~/components/MintWindow";
 import { Address } from "~~/components/scaffold-eth";
 import NerdSign from "~~/components/nerdSign";
+import { Howl, Howler } from 'howler';
 
 const Home: NextPage = () => {
     const { address: connectedAddress } = useAccount();
+    Howler.volume(0.05);
+    var sound = new Howl({
+        src: ['vibes.mp3']
+    });
+
+    sound.play();
     return (
         <>
             <div className="flex items-center flex-col flex-grow pt-10 bgimg font-caveat">
