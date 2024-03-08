3c4693767e7557adbd24bbfb1592a495155fefe7
at0x123 <vp.ca94@gmail.com>
2024-03-07 23:13:30 -0500

    latest

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -9,27 +9,29 @@ import MintWindow from "~~/components/MintWindow";
 import { Address } from "~~/components/scaffold-eth";
 
 const Home: NextPage = () => {
-  const { address: connectedAddress } = useAccount();
-  return (
-    <>
-      <div className="flex items-center flex-col flex-grow pt-10 bgimg">
-        <div className="px-5">
-          <p className="text-center text-lg">
-            gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm <br />
-            gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm <br />
-            gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm <br />
-            gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm <br />
-            gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm <br />
-          </p>
-        </div>
-        <div className="flex justify-center items-center gap-12 flex-col sm:flex-row">
-          <p>Dropup</p>
-          <Dropup contentId="8888" />
-          <MintWindow />
-        </div>
-      </div>
-    </>
-  );
+    const { address: connectedAddress } = useAccount();
+    return (
+        <>
+            <div className="flex items-center flex-col flex-grow pt-10 bgimg font-caveat">
+                <div className="px-5">
+
+                    <div className="bg-[url(/enjoy.png)] bg-fill h-96 w-full top-0" />
+                    <p className="text-center text-lg font-caveat">
+                        gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm <br />
+                        gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm <br />
+                        gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm <br />
+                        gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm <br />
+                        gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm gm <br />
+                    </p>
+                </div>
+                <div className="flex justify-center items-center gap-12 flex-col sm:flex-row">
+                    <p>Dropup</p>
+                    <Dropup contentId="8888" />
+                    <MintWindow />
+                </div>
+            </div>
+        </>
+    );
 };
 
 export default Home;
