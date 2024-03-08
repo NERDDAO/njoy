8aa4b8205721118bf58c04e354b68069e9cf8639
at0x123 <vp.ca94@gmail.com>
2024-03-07 21:51:17 -0500

    text

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -92,8 +92,8 @@ export const Header = () => {
                         <Image alt="SE2 logo" className="cursor-pointer" fill src="/iexp.png" />
                     </div>
                     <div className="flex flex-col">
-                        <span className="font-bold leading-tight">Scaffold-ETH</span>
-                        <span className="text-xs">Ethereum dev stack</span>
+                        <span className="font-bold leading-tight">Enjoy NFTs</span>
+                        <span className="text-xs">Art for Dejen Enjoyoors</span>
                     </div>
                 </Link>
                 <ul className="hidden lg:flex lg:flex-nowrap menu menu-horizontal px-1 gap-2">
